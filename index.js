2f32f8ebf7a0d901adca8a825e1080097bfd74ca
ajay <96565223+ajaynarcos@users.noreply.github.com>
2022-08-24 14:42:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// this small program tells us emploes data
+
+console.log("know we starting javascript");
+ 
+let a = "ajay-narcos";
+
+function ajay() {
+    document.getElementById(demo1).innerHTML = a;//"gender:male , study: gndu , position: mananger , salary : 50000 , hobby: programming";
+
+}
+
+
+function rohit() {
+    document.getElementById(demo2).innerHTML = a;
+
+
+}
+
+function rahul() {
+    document.getElementById(demo3).innerHTML = a;
+
+} 
+function steve() {
+    document.getElementById(demo4).innerHTML = a;
+
+} 
+
+function jyoti() {
+    document.getElementById(demo5).innerHTML = a;
+
+
+}
